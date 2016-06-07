ccdb400fa066edb1ae5c8672dd55a9040e0c0f28
Sherif Soliman <sherif@ssoliman.com>
2016-06-06 19:49:32 -0700

    Update tarball download url in setup.py.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author_email = 'sherif@ssoliman.com',
     copyright = 'Copyright (c) 2016 Sherif Soliman',
     url = 'https://github.com/sheriferson/simplestatistics',
-    download_url = 'https://github.com/sheriferson/simplestatistics/tarball/0.1',
+    download_url = 'https://github.com/sheriferson/simplestatistics/tarball/0.1.3',
     keywords = ['statistics', 'math'],
     classifiers = ['Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
