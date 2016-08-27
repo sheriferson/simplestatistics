3006c398d518b7d2223fbb7a656a26a3ae86279e
Sherif Soliman <sherif@ssoliman.com>
2016-08-27 11:54:55 -0700

    Prep for 0.2.0 release.

--- setup.py
+++ setup.py
@@ -3,15 +3,25 @@ from distutils.core import setup
 setup(
     name = 'simplestatistics',
     packages = ['simplestatistics', 'simplestatistics.statistics'],
-    version = '0.1.5',
+    version = '0.2.0',
     description = 'Simple statistical functions implemented in readable Python.',
     author = 'Sherif Soliman',
     author_email = 'sherif@ssoliman.com',
     copyright = 'Copyright (c) 2016 Sherif Soliman',
     url = 'https://github.com/sheriferson/simplestatistics',
-    download_url = 'https://github.com/sheriferson/simplestatistics/tarball/0.1.5',
+    download_url = 'https://github.com/sheriferson/simplestatistics/tarball/0.2.0',
     keywords = ['statistics', 'math'],
-    classifiers = ['Programming Language :: Python :: 2',
+    classifiers = [
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
-        'Topic :: Scientific/Engineering :: Mathematics']
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Science/Research',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Topic :: Education',
+        'Topic :: Utilities'
+        ]
 )
