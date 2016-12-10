e6adf0b36e260f60dc08d4fa07039b9f65f037fc
Sherif Soliman <sherif@ssoliman.com>
2016-12-10 15:01:14 -0500

    Adds more tests and doc about difference between correlation and covariance.

--- simplestatistics/statistics/correlate.py
+++ simplestatistics/statistics/correlate.py
@@ -16,6 +16,15 @@ def correlate(x, y):
     :math:`r` of 1.0 is the strongest positive correlation between two
     variables, and an :math:`r` of -1.0 is the strongest negative correlation.
 
+    .. _Covariance: https://en.wikipedia.org/wiki/Covariance
+
+    This `Cross Validated answer`_ provides a good explanation of the difference between
+    covariance and correlation. Covariance is understood in the context of the units and scales involved.
+    You cannot compare covariances across those contexts. A correlation is a "normalized" covariance
+    that will always be a value between -1 and 1 and takes into account the scale of the variables.
+
+    .. _`Cross Validated answer`: http://stats.stackexchange.com/a/18089
+
     Equation:
         .. math::
             r_x,_y = \\frac{\\sum\\limits_{i=1}^n (x_i - \\bar{x})(y_i - \\bar{y})}{ns_x s_y}
@@ -39,7 +48,28 @@ def correlate(x, y):
         0.9486666666666667
         >>> correlate([2, 1, 0, -1, -2, -3, -4, -5], [0, 1, 1, 2, 3, 2, 4, 5])
         -0.9434285714285714
+
+        >>> correlate(2, 3)
+        Traceback (most recent call last):
+            ...
+        ValueError: To calculate correlation you need lists or tuples of equal length. Length must be > 1.
+        >>> correlate([2, 4], [6, 6.5, 7])
+        Traceback (most recent call last):
+            ...
+        ValueError: To calculate correlation you need lists or tuples of equal length. Length must be > 1.
+        >>> correlate([1], [-1])
+        Traceback (most recent call last):
+            ...
+        ValueError: Correlation requires lists of equal length where length is > 1.
     """
+    if type(x) not in [list, tuple] or type(y) not in [list, tuple]:
+        raise ValueError("To calculate correlation you need lists or tuples of equal length. Length must be > 1.")
+
+    if len(x) != len(y):
+        raise ValueError("To calculate correlation you need lists or tuples of equal length. Length must be > 1.")
+
+    if len(x) <= 1 or len(y) <= 1:
+        raise ValueError("Correlation requires lists of equal length where length is > 1.")
 
     x = z_scores(x)
     y = z_scores(y)
